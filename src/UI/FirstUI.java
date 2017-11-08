087ea0353faff30a6e6f062a80797a097ecdddb8
Sawaphob Chavana <sawaphob@hotmail.com>
2017-11-08 15:51:02 +0700

    Update firstUI

--- src/UI/FirstUI.java
+++ src/UI/FirstUI.java
@@ -1,4 +1,8 @@
 package UI;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import javafx.animation.Animation;
 import javafx.animation.FadeTransition;
 import javafx.animation.KeyFrame;
@@ -6,10 +10,16 @@ import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -20,14 +30,20 @@ public class FirstUI extends Application {
     ImageView[] imgView = new ImageView[4];
     int imgIndex = 0;
 
-    public void start(Stage stage) {
-
+    public void start(Stage stage) throws FileNotFoundException {
+    		StackPane spane = new StackPane();
+    		spane.setAlignment(Pos.CENTER);
+    		Text text = new Text("Welcome to Starbucks's Store");
+    		final Font f = Font.loadFont(new FileInputStream(new File("resources/font/the_King__26_Queen_font.ttf")), 40);
+    		text.setFont(f);
+//    		text.setFont(font.loadFont(new FileInputStream(new File("file:resources/font/the_King__26_Queen_font.ttf")), 12);
+//    		text.setFont(new Font("file:resources/font/the_King__26_Queen_font.ttf",40));
         Pane pane = new Pane();
 
-            imgView[3] = new ImageView(new Image("https://raw.githubusercontent.com/Sawaphob/JavaProject/master/photo/First%20Page/1.jpg"));
-            imgView[1] = new ImageView(new Image("https://raw.githubusercontent.com/Sawaphob/JavaProject/master/photo/First%20Page/2.jpg"));
-            imgView[2] = new ImageView(new Image("https://raw.githubusercontent.com/Sawaphob/JavaProject/master/photo/First%20Page/3.jpg"));
-            imgView[0] = new ImageView(new Image("https://raw.githubusercontent.com/Sawaphob/JavaProject/master/photo/First%20Page/4.jpg"));
+            imgView[0] = new ImageView(new Image("file:resources/photo/FirstPage/1.jpg"));
+            imgView[1] = new ImageView(new Image("file:resources/photo/FirstPage/2.jpg"));
+            imgView[2] = new ImageView(new Image("file:resources/photo/FirstPage/3.jpg"));
+            imgView[3] = new ImageView(new Image("file:resources/photo/FirstPage/4.jpg"));
             for(int i = 0 ; i<4;i++) {
             imgView[i].setFitWidth(1050);
             imgView[i].setFitHeight(700);
@@ -62,7 +78,8 @@ public class FirstUI extends Application {
 
         animation.setCycleCount(Timeline.INDEFINITE);
         animation.play();
-        Scene scene = new Scene(pane);
+        spane.getChildren().addAll(pane,text);
+        Scene scene = new Scene(spane);
         stage.setScene(scene);
         stage.setTitle("Starbuck Cooperation");
         stage.show();
